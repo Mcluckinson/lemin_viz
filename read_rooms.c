7f2898c950795783c273d025f7ad69b6252eec55
Vladislav <clothor-@student.21-school.ru>
2020-10-18 20:03:33 +0300

    Fix for is_generated_map

--- read_rooms.c
+++ read_rooms.c
@@ -83,6 +83,8 @@ int				read_rooms(t_all_data *data)
 		{
 			if (!start_end_check(line, &rooms, data))
 				return (0);
+			if (str_contains(line, "number of lines"))
+				data->is_generated_map = true;
 			continue ;
 		}
 		if (is_room(line))
