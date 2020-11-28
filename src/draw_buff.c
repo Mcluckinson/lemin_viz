8f0ccee1cfff055c88039aec0a2397e318618bbb
Carly Yuriko <cyuriko@mi-r1.msk.21-school.ru>
2020-11-28 21:11:09 +0300

    removed unused

--- src/draw_buff.c
+++ src/draw_buff.c
@@ -27,12 +27,6 @@ void		draw_line_in_buff(Uint32 color, Uint32 *buff, int x0, int x1, int y, t_sdl
 	}
 }
 
-void 		draw_stoopid_line_in_buff(int x, int y, int radius, t_sdl_things *things, Uint32 color)
-{
-	if (x >= 0 && x < things->width && y < things->height)
-		things->m_buffer1[y * things->width + x] = color;
-}
-
 static void draw_fat_horizontal_line_in_buff(int x, int y, int radius, t_sdl_things *things, Uint32 color)
 {
 	int counter;
@@ -111,7 +105,6 @@ void		draw_brezenham_line_in_buff(t_sdl_things *things, int x0, int y0, int x1, 
 	int ystep = (y0 < y1) ? 1 : -1;
 	while(x<x1)
 	{
-//		draw_stoopid_line_in_buff(steep ? y : x, steep ? x : y, radius, things, color);
 		steep ? draw_fat_horizontal_line_in_buff(steep ? y : x, steep ? x : y, radius, things, color) : draw_fat_vertical_line_in_buff(steep ? y : x, steep ? x : y, radius, things, color);
 		error -= dy;
 		if (error < 0)
