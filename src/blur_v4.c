ecf101f316c4a821b6936f3f16aecd5736ffaf65
Carly Yuriko <cyuriko@mi-r1.msk.21-school.ru>
2020-11-29 20:24:41 +0300

    normed blur

--- src/blur_v4.c
+++ src/blur_v4.c
@@ -10,10 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "lem_viz.h"
 
-static void 	swap_buffs(t_sdl_things *things)
+static void		swap_buffs(t_sdl_things *things)
 {
 	Uint32 *tmp;
 
@@ -22,50 +21,73 @@ static void 	swap_buffs(t_sdl_things *things)
 	things->m_buffer2 = tmp;
 }
 
-void	blur_v4(t_sdl_things *things)
+static void		init_params
+	(int *red_total, int *green_total, int *blue_total, int *box_blur_x)
 {
+	*red_total = 0;
+	*green_total = 0;
+	*blue_total = 0;
+	*box_blur_x = -BLUR_EXTENT - 1;
+}
 
-	swap_buffs(things);
-	int current_x;
-	int current_y;
-	int y = -1;
-	int x;
-	int box_blur_x;
-	Uint8 red, green, blue;
-	int redTotal, greenTotal, blueTotal;
+static bool		check_coords(t_sdl_things *things, int box_blur_x)
+{
+	if (things->xy.y >= 0 && things->xy.x + box_blur_x >= 0 &&
+		things->xy.y < things->height
+		&& things->xy.x + box_blur_x < things->width)
+		return (true);
+	return (false);
+}
+
+static void		update_color
+	(Uint8 *red, Uint8 *green, Uint8 *blue, t_sdl_things *things)
+{
+	Uint32	color;
+	int		box_blur_x;
+	int		red_total;
+	int		green_total;
+	int		blue_total;
+
+	init_params(&red_total, &blue_total, &green_total, &box_blur_x);
+	while (box_blur_x++ <= BLUR_EXTENT)
+	{
+		if (check_coords(things, box_blur_x))
+		{
+			color = things->m_buffer2[things->xy.y
+					* things->width + things->xy.x + box_blur_x];
+			*red = color;
+			*green = color >> 8;
+			*blue = color >> 16;
+			red_total += *red;
+			green_total += *green;
+			blue_total += *blue;
+		}
+		*red = red_total / 9;
+		*green = green_total / 9;
+		*blue = blue_total / 9;
+	}
+}
+
+void			blur_v4(t_sdl_things *things)
+{
+	t_xy	xy;
+	Uint8	red;
+	Uint8	green;
+	Uint8	blue;
 
 	if (things->radius < 2)
 		return ;
-	while (y++ < things->height)
+	swap_buffs(things);
+	xy.y = -1;
+	while (xy.y++ < things->height)
 	{
-		x = -1;
-		while (x++ < things->width)
+		xy.x = -1;
+		while (xy.x++ < things->width)
 		{
-			box_blur_x = -BLUR_EXTENT - 1;
-			redTotal = 0;
-			greenTotal = 0;
-			blueTotal = 0;
-			while (box_blur_x++ <= BLUR_EXTENT)
-			{
-				current_x = x + box_blur_x;
-				current_y = y;
-
-				if (y >= 0 && x + box_blur_x >= 0 &&
-				y < things->height && x + box_blur_x < things->width)
-				{
-					red = things->m_buffer2[current_y * things->width + current_x];
-					green = red >> 8;
-					blue = red >> 16;
-
-					redTotal += red;
-					greenTotal += green;
-					blueTotal += blue;
-				}
-				red = redTotal / 9;
-				green = greenTotal / 9;
-				blue = blueTotal / 9;
-			}
-			things->m_buffer1[y * things->width + x] = red  | green << 8 | blue << 16;
+			things->xy = xy;
+			update_color(&red, &green, &blue, things);
+			things->m_buffer1[xy.y * things->width + xy.x] =
+					red | green << 8 | blue << 16;
 		}
 	}
 }
